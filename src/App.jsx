173b0b524b22ef34b31d17fc4baeffb6cb29dff9
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-28 18:57:20 -0400

    Import Services component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Hero from './components/Hero'
 import './components/Hero.css'
+import Services from './components/Services'
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      
+      <Services />
     </div>
   )
 }
