08716c44169b69c364ef47e4c3b786dbe4c6fda0
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-28 17:52:52 -0400

    Build Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,10 @@ export default function Hero() {
   return (
     <div className='hero'>
       <Navbar />
-      
+      <div className='hero-text'>
+        <h1 className='hero--heading'>Don't wait until you experience pain. Receive preventative treatment today.</h1>
+        <button className='hero--button'>Make Appointment</button>
+      </div>
     </div>
   )
 }
