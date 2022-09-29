7f65c3bb746bae1d20d6181f57741ddcca7a47a4
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-29 18:28:37 -0400

    Add video and update content for services section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import './Navbar.css'
+import Video from '../assets/video1.mp4' 
 
 export default function Hero() {
   return (
     <div className='hero'>
+      <video controls loop muted autoplay reload="auto" >
+        <source src={Video} type="video/mp4"/> Your browser does not support the video tag.
+        </video>
       <Navbar />
       <div className='hero-text'>
         <h1 className='hero--heading'>Don't wait until you experience pain. Receive preventative treatment today.</h1>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -15,9 +15,8 @@ export default function Services(){
         <span>HOW I CAN HELP</span>
         <h2>Services Offered</h2>
         <div className="services-paragraph">
-          <p>Phoummavong Chiropractic & Wellness provides a full spectrum of non-surgical services to help you reach optimal health.</p>
-          <p>If you are looking to naturally lower inflammation and prevent chronic diseases, you have arrived at the right place.</p>
-          <p>Through chiropractic, myofascial release, and rehab therapy, Dr. Phoummavong will guide you on the path to health and wellness.</p>
+          <p>Phoummavong Chiropractic & Wellness strives to provide preventative health care measures with holistic treatments such as adjustments, myofascial release and electric stim therapy. </p>
+          <p>Through chiropractic and a personalized health care plan, Dr. Phoummavong will help you reach your optimal health and wellness goals.</p>
         </div>
       </section>
       <section className="services-right">
@@ -25,42 +24,42 @@ export default function Services(){
           <img className="service-icon" src={Body} alt="body" />
           <div>
             <h4 className="service-heading">Chiropractic Adjustments</h4>
-            <p className="service-description">We realign your spine using traditional, drop table, and instrument techniques.</p>
+            <p className="service-description">Realign your spine using safe, non-invasive techniques: Diversified, Thompson, instruments.</p>
           </div>
         </div>
         <div className="service">
           <img className="service-icon" src={Chiropractic} alt="chiropractic" />
           <div>
             <h4 className="service-heading">Rehab Therapy</h4>
-            <p className="service-description">Know exactly what’s going on under the hood and how to fix it with our tried and tested rehab exercises.</p>
+            <p className="service-description">Application of rehab exercises and stretches to promote optimal function in every day life.</p>
           </div>
         </div> 
         <div className="service">
           <img className="service-icon" src={Running} alt="running" />
           <div>
             <h4 className="service-heading">Sports Injury Care</h4>
-            <p className="service-description">Enhance performance, prevent injuries, and apeed up recovery times.</p>
+            <p className="service-description">Fine tune performance, prevent injuries, and speed up recovery times.</p>
           </div>
         </div>
         <div className="service">
           <img className="service-icon" src={Acupuncture} alt="acupuncture" />
           <div>
             <h4 className="service-heading">Myofascial Release</h4>
-            <p className="service-description">We reduce pain by easting the tension and tightness in your myofascial tissues.</p>
+            <p className="service-description">Reduce pain by improving mobility & relax tight fascia while improving blood and lymphatic circulation.</p>
           </div>
         </div>
         <div className="service">
           <img className="service-icon" src={Stim} alt="stim" />
           <div>
             <h4 className="service-heading">Electric Stim Therapy</h4>
-            <p className="service-description">Apply electrical current to help strengthen muscles, block pain signals and improve blood circulation.</p>
+            <p className="service-description">Application of electrical currents to promote  proprioception and improve blood circulation.</p>
           </div>
         </div>
         <div className="service">
           <img className="service-icon" src={Stretch} alt="stretch" />
           <div>
-            <h4 className="service-heading">Custom Care Plan</h4>
-            <p className="service-description">Walk away with prescribed stretches and exercises to keep your adjustments.</p>
+            <h4 className="service-heading">Personalized Care Plan</h4>
+            <p className="service-description">Customized care plans for each individual patients that address your goals.</p>
           </div>
         </div>
       </section>
