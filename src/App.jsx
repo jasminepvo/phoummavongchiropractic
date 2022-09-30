e2ef5bc12656bd1d4629b7968fbfc67d3cb97fc6
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-29 22:50:36 -0400

    Attempt to add carousel slider starting code from React

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import Hero from './components/Hero'
-import './components/Hero.css'
-import Services from './components/Services'
-import About from './components/About'
-import WhatToExpect from './components/WhatToExpect'
+import "./App.css";
+import Hero from "./components/Hero";
+import "./components/Hero.css";
+import Services from "./components/Services";
+import About from "./components/About";
+import WhatToExpect from "./components/WhatToExpect";
+import Testimonials from "./components/Testimonials";
 
 function App() {
-  return (
-    <div className="App">
-      <Hero />
-      <Services />
-      <About />
-      <WhatToExpect />
-    </div>
-  )
+	return (
+		<div className="App">
+			<Hero />
+			<Services />
+			<About />
+			<WhatToExpect />
+			{/* <Testimonials /> */}
+		</div>
+	);
 }
 
-export default App
+export default App;
